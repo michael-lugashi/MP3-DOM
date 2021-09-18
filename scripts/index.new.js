d40526f1070bea7360b42787e73452e9824f05bb
michael-lugashi <89575620+michael-lugashi@users.noreply.github.com>
2021-09-18 19:12:12 +0300

    Functions and Styling From the Last Task
    I added the functions and styling from the last task and made changed it a little to better suit this task

--- scripts/index.new.js
+++ scripts/index.new.js
@@ -4,8 +4,23 @@
  *
  * @param {Number} songId - the ID of the song to play
  */
+
+function nextSong(songId) {
+    if (findIndex(songId, player.songs) + 1 < player.songs.length) {
+        const next = player.songs[findIndex(songId, player.songs) + 1].id
+        playSong(next)
+    } else {
+        const lastSong = document.getElementsByClassName("songSelected")
+        lastSong[0].classList.remove("songSelected")
+    }
+}
+
 function playSong(songId) {
-    // Your code here
+    const song = document.getElementById(`song${songId}`)
+    const lastSong = document.getElementsByClassName("songSelected")
+    if (lastSong[0]) lastSong[0].classList.remove("songSelected")
+    song.classList.add("songSelected")
+    setTimeout(nextSong, 1000 * player.songs[findIndex(songId, player.songs)].duration, songId)
 }
 
 /**
@@ -47,22 +62,27 @@ function handleAddSongEvent(event) {
  * Creates a song DOM element based on a song object.
  */
 function createSongElement({ id, title, album, artist, duration, coverArt }) {
-    const children = []
-    const classes = []
-    const attrs = {}
-    const eventListeners = {}
-    return createElement("div", children, classes, attrs, eventListeners)
+    const coverEl = createElement("img", [], ["img-format"], { src: coverArt })
+    const titleEl = createElement("p", [title], ["width"])
+    const albumEL = createElement("p", [album], ["width"])
+    const artistEl = createElement("p", [artist], ["width"])
+    const durEl = createElement("p", [convertToMin(duration)], ["width"])
+    return createElement("div", [coverEl, titleEl, albumEL, artistEl, durEl], ["song"], {
+        onclick: `playSong(${id})`,
+        id: `song${id}`,
+    })
 }
-
 /**
  * Creates a playlist DOM element based on a playlist object.
  */
 function createPlaylistElement({ id, name, songs }) {
-    const children = []
-    const classes = []
-    const attrs = {}
-    const eventListeners = {}
-    return createElement("div", children, classes, attrs, eventListeners)
+    const nameEl = createElement("span", [name], [])
+    const numSongs = createElement("span", [`Songs: ${songs.length}`], [])
+    const playlistDurationEl = createElement("span", [playlistDuration(id)], [], {})
+    return createElement("div", [nameEl, numSongs, playlistDurationEl], ["playlists"], {
+        onclick: `playPlaylist(${id})`,
+        id: `playlist${id}`,
+    })
 }
 
 /**
@@ -78,23 +98,70 @@ function createPlaylistElement({ id, name, songs }) {
  * @param {Object} attributes - the attributes for the new element
  * @param {Object} eventListeners - the event listeners on the element
  */
-function createElement(tagName, children = [], classes = [], attributes = {}, eventListeners = {}) {
-    // Your code here
+function createElement(tagName, children = [], classes = [], attributes = {}) {
+    // creates the element
+    let element = document.createElement(tagName)
+
+    // adds all the classes to the element
+    for (const cls of classes) {
+        element.classList.add(cls)
+    }
+    // adds attributes to the element
+    for (const attribute in attributes) {
+        element.setAttribute(attribute, attributes[attribute])
+    }
+    // adds all the children elements
+    for (const child of children) {
+        element.append(child)
+    }
+    return element
 }
 
 /**
  * Inserts all songs in the player as DOM elements into the songs list.
  */
 function generateSongs() {
-    // Your code here
+    const songsHtml = document.getElementById("songs")
+
+    player.songs.sort((SongA, SongB) => SongA.title.localeCompare(SongB.title))
+
+    for (let song of player.songs) {
+        songsHtml.appendChild(createSongElement(song))
+    }
 }
 
 /**
  * Inserts all playlists in the player as DOM elements into the playlists list.
  */
 function generatePlaylists() {
-    // Your code here
+    const playliststHtml = document.getElementById("playlists")
+
+    player.playlists.sort((playlistA, playlistB) => playlistA.name.localeCompare(playlistB.name))
+
+    for (let playlist of player.playlists) {
+        playliststHtml.appendChild(createPlaylistElement(playlist))
+    }
 }
+
+/* Functions that aid and aquire information for my about functions */
+
+function convertToMin(seconds) {
+    const min = Math.floor(seconds / 60)
+    const sec = seconds % 60
+    return `${min < 10 ? "0" + min : min}:${sec < 10 ? "0" + sec : sec}`
+}
+
+function findIndex(id, location) {
+    return location.findIndex((listElement) => listElement.id === id)
+}
+
+function playlistDuration(id) {
+    let totalDuration = 0
+    const playlist = player.playlists[findIndex(id, player.playlists)]
+    playlist.songs.forEach((songId) => (totalDuration += player.songs[findIndex(songId, player.songs)].duration))
+    return convertToMin(totalDuration)
+}
+
 
 // Creating the page structure
 generateSongs()
